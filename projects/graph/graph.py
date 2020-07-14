ed04dcd573cd7dbf4e0ce1041d7f0d5cb8fe0507
christina kaliora <develop@pirron.one>
2020-07-14 09:49:46 +0200

    initial commit

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,7 +3,7 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
-class Graph:
+class Graph: 
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
